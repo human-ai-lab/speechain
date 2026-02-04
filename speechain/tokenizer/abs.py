97cfafc1487f80db50299db78bd1e578a2e19524
rughimire <rughimire@gmail.com>
2026-02-04 17:53:53 +0900

    documentation fix

--- speechain/tokenizer/abs.py
+++ speechain/tokenizer/abs.py
@@ -93,7 +93,7 @@ class Tokenizer(ABC):
         """
         pass
 
-    def tensor2text(self, tensor: torch.LongTensor) -> str:
+    def tensor2text(self, tensor: any) -> str:
         """This functions decodes a text tensor into a human-friendly string.
 
         The default implementation transforms each token index in the input tensor to the token string by `
@@ -104,14 +104,13 @@ class Tokenizer(ABC):
         to decode the input tensor rather than the built-in `self.idx2token`, please override this function.
 
         Args:
-            tensor: torch.LongTensor
-                1D integer torch.Tensor that contains the token indices of the sentence to be decoded.
+            tensor: torch.LongTensor or list
+                1D integer torch.Tensor or list that contains the token indices of the sentence to be decoded.
 
         Returns:
             The string of the decoded sentence.
         """
         token_list = []
-        # sometimes there are non-tensor also, need fix
         if hasattr(tensor, "tolist"):
             seq = tensor.tolist()
         else:
